b28d9a30ec09817129fa1e795978bdc6e23c1e67
Jotham Teshome <teshomej@msu.edu>
2024-04-08 13:40:56 -0400

    Fixed bounding box issue

--- main.py
+++ main.py
@@ -1,62 +1,83 @@
 import cv2
 import numpy as np
 
-def showImage(img):
-    cv2.imshow('image', img)
+def showImage(img, title='image'):
+    cv2.imshow(title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def checkChanges(diff, x, y, w, h, line_width):
+def checkChanges(diff, line_width, detection_area, ):
+    x, y, w, h = detection_area
+
+    # Crop image to only show changes in detection area
     crop_img = diff[y+line_width:y+h-line_width, x+line_width:x+w-line_width]
 
-    # for i in range(crop_img.shape[0]):
-    #     for j in range(crop_img.shape[1]):
-    #         for chan in range(crop_img.shape[2]):
-    #             if crop_img[i][j][chan] > 10:
-    #                 crop_img[i][j][chan] = 255
-
-    # showImage(crop_img)
-
     contours, _ = cv2.findContours(crop_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    highlightChanges(crop_img, contours, 0, 0, 0, 0)
-    
-    # showImage(crop_img)
 
     return contours
 
 
-def highlightChanges(curr, contours, x, y, w, h):
+def highlightChanges(curr, contours, line_width, detection_area=(0, 0, 0, 0)):
+    # Separate out coordinates of detection area
+    x, y, _, _ = detection_area
+
+    # Plot rectangles from contours of detected changes
     for c in contours:
         rect = cv2.boundingRect(c)
 
         r_x, r_y, r_w, r_h = rect
-        cv2.rectangle(curr, (x, y), (r_x+r_w, r_y+r_h), (0, 255, 0), 1)
+
+        # Compute top left and bottom right point of rectangle
+        left_x, left_y = x+line_width+r_x, y+line_width+r_y
+        right_x, right_y = x-line_width+r_x+r_w, y-line_width+r_y+r_h
+
+        # Plot rectangles
+        cv2.rectangle(curr, (left_x, left_y), (right_x, right_y), (0, 255, 0), 1)
+
+
+def processConsecutiveFrames(prev, curr):
+    # Take absolute difference of two frames
+    diff = cv2.absdiff(prev, curr)
+
+    # Convert difference of images to grayscale
+    diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+
+
+    # Apply filters for denoising?
+    diff = cv2.erode(diff, cv2.getStructuringElement(cv2.MORPH_RECT, (6, 6)))
+
+    # Show changes between two frames
+    showImage(diff*10, 'Difference in Frames')
+
+    return diff
 
 
 def main():
     # Open image (replace with video feed)
     prev = cv2.imread('data/image.jpg')
-    curr = cv2.imread('data/image - Copy.jpg')
+    curr = cv2.imread('data/image_cpy.jpg')
     line_width = 2
 
-    if curr is not None and prev is not None:
-    # Take absolute difference between frame 1 and frame 0
-        diff = cv2.cvtColor(cv2.absdiff(prev, curr), cv2.COLOR_BGR2GRAY)
-        diff = cv2.erode(diff, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
+    # Make sure frames are not none before detecting changes
+    if curr is None or prev is None:
+        raise TypeError('Attempting to compare two frames but only one provided')
 
-        # diff = diff * 10
-        showImage(diff)
+    diff = processConsecutiveFrames(prev, curr)
 
-        # Define bounding box area or object detection
-        x, y, w, h = (50, 150, 200, 100)
-        cv2.rectangle(diff, (x, y), (x+w, y+h), (0, 0, 255), line_width)
+    # Define bounding box area or object detection
+    detection_area = (50, 150, 200, 100)
+    x, y, w, h = detection_area
 
-        contours = checkChanges(diff, x, y ,w, h, line_width)
-        highlightChanges(curr, contours, x, y, w, h)
+    # Plot rectangle of detection area in red
+    cv2.rectangle(curr, (x, y), (x+w, y+h), (0, 0, 255), line_width)
 
-        # showImage(curr)
+    # Find objects in detection area and put green box around them
+    contours = checkChanges(diff, line_width, detection_area)
+    highlightChanges(curr, contours, line_width, detection_area)
+
+    # Show detected object within detection area
+    showImage(curr, 'Detected differences')
 
 
 if __name__ == "__main__":
